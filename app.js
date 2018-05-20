7b853b1d29de058dabe0db908df78178b74d7886
JSmith <jaysmit158@gmail.com>
2018-05-19 20:40:04 -0400

    started extending layout pug file

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cards', (req, res) => {
-	res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
+	res.render('card', 
+		{
+		prompt: "Who is buried in Grant's tomb?",
+		 hint: "Think about whose tomb it is."
+		}
+	);
 
 });
 
