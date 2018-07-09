49f74d70398e19a9b95427be2b4f42135d2343ef
JSmith <jaysmit158@gmail.com>
2018-07-08 21:52:11 -0400

    moved routes to router folder

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(cookieParser());
-
-app.set('view engine', 'pug');
-
-// app.use((req,res,next) => {
-// 	console.log("hello");
-// 	const err = new Error("Oh No!")
-// 	err.status = 500;
-// 	next(err);
-// });
+const router = express.router();
 
 app.get('/', (req, res) => {
 	const name = req.cookies.username;
@@ -57,17 +42,3 @@ app.post('/goodbye', (req, res) => {
 	res.redirect('/hello');
 
 });
-
-app.use((req, res, next) => {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-	});
-
-app.use((err, req, res, next) => {
-	res.locals.error = err;
-	res.status(err.status);
-	res.render('error');
-	});
-
-app.listen(3000, () => {console.log('app is running on port 3000!');});
