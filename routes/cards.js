0a94cdecf99c70d86f4f371efb76a6e372132f49
JSmith <jaysmit158@gmail.com>
2018-07-18 20:03:10 -0400

    object destructuring

--- routes/cards.js
+++ routes/cards.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data/flashcardData.json').data;
-const cards = data.cards;
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
 
 // const { data } = require('../data/flashcardData.json');
 // const { cards } = data;
 
 router.get('/:id', (req, res) => {
-	const side = req.query.side;
-	const id = req.params.id;
-	const text = cards[id][side];
-	const hint = cards[id].hint;
-	const templateData = { text, hint };
+	const { side } = req.query.side;
+	const { id } = req.params.id;
+	const { text } = cards[id][side];
+	const { hint } = cards[id].hint;
+	const templateData = { text };
+
+	if ()
 	res.render('card', templateData);
 
 });
